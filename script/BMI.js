bd1cce6ce95a1b7322d83fa35b044b3d5a5f84ce
demilee06 <chloealmazien6@gmail.com>
2024-05-17 16:20:46 +0200

    added text

---
+++ script/BMI.js
@@ -0,0 +1,7 @@
+let button = document.querySelector('.answer')
+let input = document.querySelectorAll('.total')
+
+button.addEventListener('click',()=>{
+    total.value = eval(`${total.value}`)
+})
+
