d1c48407c17626921b1d453a052ecb3b4fd4f114
demilee06 <chloealmazien6@gmail.com>
2024-05-20 12:02:51 +0200

    added text

--- script/BMI.js
+++ script/BMI.js
@@ -1,7 +1,10 @@
 let button = document.querySelector('.answer')
-let input = document.querySelectorAll('.total')
+let input1 = document.querySelectorAll('.total')[0]
+let input2 = document.querySelectorAll('.total')[1]
+let label = document.querySelector('.BMI')
+
 
 button.addEventListener('click',()=>{
-    total.value = eval(`${total.value}`)
+    label.innerText =(eval(`${input1.value}/(((${input2.value}) / 100) **2)`)).toFixed(2)
 })
 
